ffe71b2d3f9072b2e0019e69139f9ca66b3adcd0
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-07-21 02:19:18 +0200

    Initial commit starter files

---
+++ src/App.js
@@ -0,0 +1,45 @@
+export default function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>HATS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>JACKETS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>SNEAKERS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>WOMENS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>MENS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
